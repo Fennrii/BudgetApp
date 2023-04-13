3b2248cd42c4ef3ed354cd3632346f54aa036844
clintsullins <chsull6652@ung.edu>
2023-04-13 18:26:47 -0400

    Lite-Budgeting

---
+++ app/src/main/java/com/example/litebudgeting/IncomeScreen.java
@@ -0,0 +1,16 @@
+package com.example.litebudgeting;
+
+import android.os.Build;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class IncomeScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.income_screen);
+
+    }
+}
