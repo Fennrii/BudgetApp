73b470cf0f835e68bc1dd4f9678c9f0b96c2f058
Fennrii <axsato7395@ung.edu>
2023-04-20 19:00:45 -0400

    Implemented Robert's IncomeFragment and fixed a bit of HomeFragment

--- app/src/main/java/com/example/litebudgeting/ui/income/IncomeFragment.java
+++ app/src/main/java/com/example/litebudgeting/ui/income/IncomeFragment.java
@@ -1,25 +1,31 @@
 package com.example.litebudgeting.ui.income;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.litebudgeting.AddJob;
+import com.example.litebudgeting.NavMain;
+import com.example.litebudgeting.R;
 import com.example.litebudgeting.databinding.FragmentIncomeBinding;
 
 public class IncomeFragment extends Fragment {
 
     private FragmentIncomeBinding binding;
+    private Button btnAddJobs;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        IncomeViewModel incomeViewModel =
-                new ViewModelProvider(this).get(IncomeViewModel.class);
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        IncomeViewModel incomeViewModel = new ViewModelProvider(this).get(IncomeViewModel.class);
 
         binding = FragmentIncomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
@@ -28,6 +34,21 @@ public class IncomeFragment extends Fragment {
         incomeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        btnAddJobs = view.findViewById(R.id.btnAddJobs);
+        btnAddJobs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), AddJob.class);
+                startActivity(intent);
+            }
+        });
+    }
+
 
     @Override
     public void onDestroyView() {
