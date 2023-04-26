3dad9cef5d26524c92c0c396fe71bcbf2d4ff8c8
clintsullins <chsull6652@ung.edu>
2023-04-25 22:05:18 -0400

    App

--- app/src/main/java/com/example/litebudgeting/MainActivity.java
+++ app/src/main/java/com/example/litebudgeting/MainActivity.java
@@ -1,19 +1,78 @@
 package com.example.litebudgeting;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setupButton();
+
+       createNotificationChannel();
+
+        new CountDownTimer(900000, 1) {
+
+            @Override
+            public void onTick(long millisUntilFinished) {
+
+            }
+
+            public void onFinish() {
+                triggerNotification();
+            }
+
+        }.start();
+    }
+
+
+    private void createNotificationChannel ()
+    {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel = new NotificationChannel(getString(R.string.notifi), getString(R.string.notifi_description), NotificationManager.IMPORTANCE_DEFAULT);
+            notificationChannel.setDescription(getString(R.string.notifi_description));
+            notificationChannel.setShowBadge(true);
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
+    }
+
+
+    private void triggerNotification (){
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE);
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this, getString(R.string.notifi));
+        mBuilder.setSmallIcon(R.drawable.money);
+        mBuilder.setContentTitle("Alert");
+        mBuilder.setContentText("Your Budgetting Needs Review");
+        mBuilder.setPriority(NotificationCompat.PRIORITY_DEFAULT);
+        mBuilder.setContentIntent(pendingIntent);
+        mBuilder.setChannelId(getString(R.string.notifi));
+        mBuilder.setAutoCancel(true);
+
+        NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(this);
+        notificationManagerCompat.notify(getResources().getInteger(R.integer.notifiID), mBuilder.build());
+
+
     }
 
     private void setupButton(){
